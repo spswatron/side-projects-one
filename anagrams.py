369c65f07e62b53feadb90e1454911c986f9109c
spswatron <spswatron@gmail.com>
2022-01-04 22:52:19 -0500

    redo mistake

--- anagrams.py
+++ anagrams.py
@@ -13,6 +13,17 @@ def add_word(node: Trie, word: str):
     node.word_end = True
 
 
+def clear_node(letters, curr_word, node):
+    if node is None or not node.visited:
+        return
+    node.visited = False
+    for i in range(len(letters)):
+        new_letters = letters[:i] + letters[i + 1:]
+        new_curr_word = curr_word + letters[i]
+        new_node = node.children[letters[i]] if letters[i] in node.children else None
+        clear_node(new_letters, new_curr_word, new_node)
+
+
 root = Trie()
 with open("./dictionary.csv", "r") as words:
     for dict_word in words:
@@ -24,7 +35,6 @@ def find_possible_words(letters, curr_word, node, possible):
         return
     node.visited = True
     if node.word_end:
-        node.word_end = False
         possible.add(curr_word)
     for i in range(len(letters)):
         new_letters = letters[:i] + letters[i + 1:]
@@ -33,11 +43,20 @@ def find_possible_words(letters, curr_word, node, possible):
         find_possible_words(new_letters, new_curr_word, new_node, possible)
 
 
-def solver(letters):
+def find_matches(letters):
     possible = set()
     find_possible_words(letters, "", root, possible)
+    clear_node(letters, "", root)
     possible = list(possible)
     possible.sort(key=len, reverse=True)
+    return possible
+
+
+def solver(letters):
+    possible = find_matches(letters)
+    print(len(possible))
+    possible = find_matches(letters)
+    print(len(possible))
     for word in possible:
         print(word)
     return possible
